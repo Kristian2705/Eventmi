a4022424d8910150c171c8e5b09af527e5bc3b92
Kristian2705 <k_petrov27@abv.bg>
2024-01-21 21:34:07 +0200

    Initial

--- Eventmi/Controllers/HomeController.cs
+++ Eventmi/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Eventmi.Controllers
             return View();
         }
 
+        public IActionResult FullPieced()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
