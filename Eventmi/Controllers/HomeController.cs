770238d30128203e1b813169580d37389aaf830a
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2024-01-22 21:10:45 +0200

    A few changes

--- Eventmi/Controllers/HomeController.cs
+++ Eventmi/Controllers/HomeController.cs
@@ -28,6 +28,11 @@
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
