387cdd07956e68f17add1607ab252912b2e88f1d
Kristian2705 <k_petrov27@abv.bg>
2024-01-23 22:38:41 +0200

    Added Attributes folder

--- Eventmi.Common/Constants/EntityValidationConstants.cs
+++ Eventmi.Common/Constants/EntityValidationConstants.cs
@@ -1,14 +1,14 @@
-﻿namespace Eventmi.Common
+﻿namespace Eventmi.Common.Constants
 {
     public static class EntityValidationConstants
-	{
-		public static class Event
-		{
-			public const int NameMaxLength = 50;
-			public const int NameMinLength = 5;
+    {
+        public static class Event
+        {
+            public const int NameMaxLength = 50;
+            public const int NameMinLength = 5;
 
-			public const int PlaceMaxLength = 50;
-			public const int PlaceMinLength = 3;
-		}
-	}
+            public const int PlaceMaxLength = 50;
+            public const int PlaceMinLength = 3;
+        }
+    }
 }
